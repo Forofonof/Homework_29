1688aeb3e4762581f523bdc36827932aa8b212e8
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-15 18:37:57 +0300

    Добавьте файлы проекта.

---
+++ Homework_29/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CheckingNumbers();
+    }
+    static int CheckingNumbers()
+    {
+        int number = 0;
+        string userInput;
+        bool isWorking = true;
+
+        while (isWorking)
+        {
+            Console.WriteLine("Введите число: ");
+            userInput = Console.ReadLine();
+
+            if (Int32.TryParse(userInput, out number))
+            {
+                Console.WriteLine(number);
+                isWorking = false;
+            }
+            else
+            {
+                Console.WriteLine("Введено неверное значение.");
+            }
+        }
+        return number;
+    }
+}
